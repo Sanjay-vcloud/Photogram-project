904f5b85ef90ec067f93f76f4eb99737e1218dc3
Sanjay-vcloud <email2sanjayv@gmail.com>
2024-01-10 08:24:27 +0000

    temp files

--- test.php
+++ test.php
@@ -1,7 +1,14 @@
 <pre>
 <?php
-
+echo "Server";
 print_r($_SERVER);
-
+echo "POST";
+print_r($_POST);
+echo "GET";
+print_r($_GET);
+echo "FILE";
+print_r($_FILES);
+echo "COOKIE";
+print_r($_COOKIE);
 ?>
 </pre>
