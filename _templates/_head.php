53f55ec8c6d6c1c1c32c1c0232556ad13bf5dd1b
Sanjay-vcloud <email2sanjayv@gmail.com>
2024-01-22 05:13:32 +0000

    loading title dynamic

--- _templates/_head.php
+++ _templates/_head.php
@@ -1,15 +1,19 @@
+<?
+$title = basename($_SERVER['PHP_SELF'],".php")
+?>
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.84.0">
-    <title>Photogram project</title>
+    <title><?=$title?></title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/album/">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha384-KyZXEAg3QhqLMpG8r+Knujsl7/1L_dstPt3HV5HzF6Gvk/e3s4Wz6iJgD/+ub2oU" crossorigin="anonymous">
-
+    
     <!-- Bootstrap core CSS -->
 <link href="/app/assets/dist/css/bootstrap.min.css" rel="stylesheet">
 <!-- css loader -->
