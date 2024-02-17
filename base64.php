33c8129a06d8ae52b79421f78b6eae948528e7f4
Sanjay-vcloud <email2sanjayv@gmail.com>
2024-02-17 10:49:08 +0000

    understanding base64

---
+++ base64.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Base64</title>
+</head>
+<body>
+<img src="data:image/png;base64," alt="Base64 Image">
+</body>
+</html>
