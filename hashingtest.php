ef6c26f2a3b965836a7117cb3e9613d4906a1474
Sanjay-vcloud <email2sanjayv@gmail.com>
2024-02-17 10:49:39 +0000

    understanding hashing

--- hashingtest.php
+++ hashingtest.php
@@ -1,17 +1,18 @@
 <?php
-$pass = "sanjay";
+$pass = isset($_GET['pass']) ? $_GET['pass'] : 'random_string';
+// $pass = "sanjay";
 $hashing = md5(strrev(md5($pass)));
-echo "The result of hashing is ". $pass;
+echo "The result of hashing is ". $hashing;
 
 
 $data = "hello";
 
 
 
-foreach (hash_algos() as $v) {
+// foreach (hash_algos() as $v) {
 
-        $r = hash($v, $data, false);
+//         $r = hash($v, $data, false);
 
-        printf("%-12s %3d %s\n", $v, strlen($r), $r);
+//         printf("%-12s %3d %s\n", $v, strlen($r), $r);
 
-}
+// }
