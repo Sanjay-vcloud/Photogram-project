1040c8b5b58d4c06f4569986a9fd4ecac2718cd0
Sanjay-vcloud <email2sanjayv@gmail.com>
2024-02-20 20:00:33 +0000

    implementing git secrets

--- libs/load.php
+++ libs/load.php
@@ -4,7 +4,24 @@ require_once "includes/mic.class.php";
 require_once "includes/database.class.php";
 require_once "includes/user.class.php";
 
+global $__site__config;
+$__site_config = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/../photogramconfig.json');
+
+
 session::start();
+
+function get_config($key, $default=null)
+{
+    global $__site_config;
+    $array = json_decode($__site_config, true);
+    if (isset($array[$key])) {
+        return $array[$key];
+    } else {
+        return $default;
+    }
+}
+
+
 
 function load_template($name)
 {
